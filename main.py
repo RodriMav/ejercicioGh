4b76f69d2e9da6fc8793c642c163c47cf6ed63c0
Maverick Valdes <maverickpinch@gmail.com>
2024-04-12 20:50:46 -0400

    Primera actividad

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-mensaje = "Hola  mundo"
+#ejercicio 1 y 2
 
-print(mensaje)
+print (" Bienvenido al mundo de la programacion")
+nom = input ("par empezar, ingresa tu nombre ")
+print(f" bienvenido {nom})")
+
+#ejecicio 3
+print("ingresa un valor 'x' y podras resolver la siguiente ecuacion")
+x = int(input("(x**2+3x+1)/4"))
+formula = (x**2+3*x+1)/4
+print(f"el resultado de la operacion con el valeor {x} es: {formula}")
+
+
+
+#ejercicio 4
+
+nombre= input("ingrese su nombre\n")
+rut = input("ingre si rut con punto y digitador")
+correo = input("ingrese su correo electronico")
+telefono = input("ingrese su numero de contacto")
+print("datos personales")
+print("NOMBRE: \t " , nombre. upper())
+print("RUT:  \t\t", rut)
+print("CORREO: \t", correo.upper())
+print("TELEFONO \t", telefono)
